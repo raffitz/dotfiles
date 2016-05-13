3bea45413e97b090866c592e2c744382d1529ae4
raffitz <raf.a.m.c.gon@gmail.com>
2016-05-13 23:56:39 +0100

    Remove submodules, start cloning repos instead

--- files/deploy.sh
+++ files/deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# Copy etc configs
 sudo cp -r etc/* /etc/
 
+# Copy usr binaries, scripts and files
 sudo cp -r usr/* /usr/
 
+# Copy home dotfiles
 cp -r home/. ~/
+
+# Clone repos
+while read in; do git clone --recursive $in ; done < git_repos
 
