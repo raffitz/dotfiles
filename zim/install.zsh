3b3be0a8501301e21200f42576d3d64863b2c549
raffitz <raf.a.m.c.gon@gmail.com>
2016-10-22 01:12:36 +0100

    Add prompt install as well

--- zim/install.zsh
+++ zim/install.zsh
@@ -11,5 +11,11 @@ for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
 	( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
 done
 
+# Clone theme:
+git clone --recursive https://github.com/raffitz/simple-s.git ${ZDOTDIR:-${HOME}}/.ztheme
+
+# Install theme:
+sudo cp ${ZDOTDIR:-${HOME}}/.ztheme/prompt_simple-s_setup /usr/share/zsh/functions/Prompts
+
 # Override .zimrc
 cat .zimrc > ${ZDOTDIR:-${HOME}}/.zimrc
